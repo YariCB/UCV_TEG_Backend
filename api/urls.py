0e66b4a4d79c13202af234858b2f3fff0760966f
YariCB <yarimacontreras.ucv@gmail.com>
2026-05-06 14:30:13 -0400

    Adding register user functionality

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from .views import(
+    register_user,
     get_material_classifications,
     get_material_dimensions,
 )
 
 urlpatterns = [
+    # Auth
+    path('auth/register/', register_user, name='register_user'),
+
     # Materials
     path('materials/classifications/', get_material_classifications, name='get_material_classifications'),
     path('materials/dimensions/', get_material_dimensions, name='get_material_dimensions'),
--- api/views.py
+++ api/views.py
@@ -1,5 +1,61 @@
+import json
+from datetime import datetime
 from django.http import JsonResponse
 from core.database_manager import get_db_connection
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.hashers import make_password
+
+
+
+# --- AUTH ---
+
+# Register user
+@csrf_exempt
+def register_user(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            first_name = data.get('firstName')
+            last_name = data.get('lastName')
+            email = data.get('email')
+            password = data.get('password')
+
+            # Password hashing
+            hashed_password = make_password(password)
+
+            registration_date = datetime.now()
+            is_active = True
+
+            conn = get_db_connection()
+            cursor = conn.cursor()
+
+            # Check if email already exists
+            check_query = "SELECT COUNT(*) FROM teg_oltp.users WHERE Email = ?"
+            cursor.execute(check_query, (email,))
+            if cursor.fetchone()[0] > 0:
+                return JsonResponse({'error': 'Este correo ya está registrado. Pruebe recuperando su contraseña'}, status=400)
+
+            query = """
+                INSERT INTO teg_oltp.users (FirstName, LastName, Email, Password, RegistrationDate, isActive)
+                VALUES (?, ?, ?, ?, ?, ?)
+            """
+            cursor.execute(query, (first_name, last_name, email, hashed_password, registration_date, is_active))
+            conn.commit()
+
+            return JsonResponse({'message': 'Usuario registrado con éxito'}, status=201)
+       
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=400)
+        
+        finally:
+            if 'conn' in locals():
+                conn.close()
+
+
+# Get user by email and password
+
+# 
+
 
 
 # --- MATERIALS VIEW ---
